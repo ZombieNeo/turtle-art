1a30737e3df565a55323fd812b677c7989b8a770
ZombieNeo <46230458+ZombieNeo@users.noreply.github.com>
2021-05-19 11:42:08 +0100

    Add files via upload

---
+++ bruh.py
@@ -0,0 +1,36 @@
+import turtle
+from random import *
+molly = turtle.Turtle()
+molly.speed(20)
+jeff = turtle.Turtle()
+dav = turtle.Turtle()
+
+jeff.speed(20)
+dazv = turtle.Turtle()
+dazv.speed(20)
+met = turtle.Turtle()
+met.speed(20)
+jack = turtle.Turtle()
+jack.speed(20)
+count = 21
+count2 = 200
+count3 = 240
+dav.speed(20)
+while True:
+    count = count - 1
+    count2 = count2 + 1
+    count3 = count3 + 1
+    jeff.right(count)
+    jeff.forward(6)
+    molly.right(count2)
+    molly.forward(-6)
+    dav.right(count3)
+    dav.forward(9)
+    jack.right(count)
+    jack.forward(12)
+    met.right(count2)
+    met.forward(-32)
+    dazv.right(count3)
+    dazv.forward(42)
+    
+    
