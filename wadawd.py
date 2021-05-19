bdcd58c4de8740433e6fd730056ee8b0a30e97e4
ZombieNeo <46230458+ZombieNeo@users.noreply.github.com>
2021-05-19 11:27:46 +0100

    clean spiraal

---
+++ wadawd.py
@@ -0,0 +1,10 @@
+import turtle as jeff
+from random import *
+jeff.speed(20)
+count = 200
+while True:
+    count = count - 1
+     #angle = randint(-90, 90)
+    jeff.right(count)
+     #jeff.right(angle)
+    jeff.forward(6)
