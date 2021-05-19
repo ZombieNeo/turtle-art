a4e8d7cfc6a2e12abef1d3eb388468d880173937
ZombieNeo <46230458+ZombieNeo@users.noreply.github.com>
2021-05-19 11:59:24 +0100

    Add files via upload

---
+++ ez.py
@@ -0,0 +1,64 @@
+import turtle
+from random import *
+molly = turtle.Turtle()
+molly.speed(20)
+jeff = turtle.Turtle()
+dav = turtle.Turtle()
+
+jeff.speed(20)
+dazv = turtle.Turtle()
+dazv.speed(20)
+met = turtle.Turtle()
+met.speed(20)
+jack = turtle.Turtle()
+jack.speed(20)
+x = 10000
+dav.speed(20)
+def lmao():
+    while True:
+
+        
+        jeff.forward(10)
+        jeff.right(45)
+        jeff.circle(100)
+        jeff.forward(-60)
+        jeff.back(29)
+        jeff.left(23)
+
+
+
+        jack.forward(-10)
+        jack.right(-45)
+        jack.circle(100)
+        jack.forward(60)
+        jack.back(-29)
+        jack.left(-23)
+
+        
+        met.forward(10)
+        met.right(45)
+        met.circle(100)
+        met.forward(-60)
+        met.back(29)
+        met.left(23)
+
+
+        dav.forward(-10)
+        dav.right(-45)
+        dav.circle(100)
+        dav.forward(60)
+        dav.back(-29)
+        dav.left(-23)
+
+
+
+        
+met.up()
+met.right(90)
+met.forward(10)
+met.down()
+dav.up()
+dav.right(-90)
+dav.forward(-10)
+dav.down()
+lmao()
