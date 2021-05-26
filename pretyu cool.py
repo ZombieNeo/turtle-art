269e6e7b7889f08ab3644d7610cbb88dd6c5dfa0
ZombieNeo <46230458+ZombieNeo@users.noreply.github.com>
2021-05-26 12:20:45 +0100

    Add files via upload

---
+++ pretyu cool.py
@@ -0,0 +1,52 @@
+from turtle import Screen
+import turtle
+
+from random import *
+molly = turtle.Turtle()
+molly.speed(20)
+jeff = turtle.Turtle()
+dav = turtle.Turtle()
+turtle.screensize(canvwidth=2000, canvheight=2000, bg=None)
+jeff.speed(20)
+dazv = turtle.Turtle()
+dazv.speed(20)
+met = turtle.Turtle()
+met.speed(20)
+jack = turtle.Turtle()
+jack.speed(20)
+color = 10
+dav.speed(20)
+def lmao():
+
+
+
+        jeff.forward(50)
+        jeff.left(5)
+        jeff.forward(50)
+        jeff.left(90)
+        jeff.forward(50)        
+        jeff.left(90)
+        jeff.forward(50)          
+
+
+
+                
+
+while True:
+    
+    lmao()
+    color = color + 1
+    print(color)
+    mod = color % 2
+    if mod > 0: 
+        jeff.pencolor("blue")
+        met.pencolor("blue")
+        jack.pencolor("blue")
+        dav.pencolor("blue")
+        pass
+    else:
+        jeff.pencolor("red")
+        jack.pencolor("red")
+        dav.pencolor("red")
+        met.pencolor("red")
+        pass
